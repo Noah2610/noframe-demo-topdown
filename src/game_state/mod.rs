21682eaac0de25bd3efae9b75b1c80a5745506f8
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2018-11-28 23:52:48 +0100

    Moved 'load_walls' method into new 'helpers' module

--- src/game_state/mod.rs
+++ src/game_state/mod.rs
@@ -1,6 +1,8 @@
+mod helpers;
+
+use self::helpers::*;
+
 use ::std::time::{ Instant, Duration };
-use ::std::fs::File;
-use ::std::io::prelude::*;
 
 use ::ggez::{
   Context,
@@ -12,7 +14,7 @@ use ::ggez::{
   }
 };
 
-use ::noframe::geo::mask::Mask;
+use ::noframe::geo::prelude::*;
 use ::noframe::entity::{
   Entity,
   Movement
@@ -24,37 +26,6 @@ use ::wall::Wall;
 
 const FPS: f32 = 30.0;
 const UPDATE_INTERVAL_MS: u64 = (1.0 / FPS * 1000.0) as u64;
-
-fn load_walls(json_filepath: &str) -> GameResult<Vec<Wall>> {
-  let mut file = File::open(json_filepath)?;
-  let mut json = String::new();
-  file.read_to_string(&mut json)?;
-  let data = match json::parse(&json) {
-    Ok(d)  => d,
-    Err(e) => return Err(ggez::GameError::from(e.to_string()))
-  };  // I don't think this is idomatic rust...
-
-  return Ok(
-    data["instances"].members().map( |d| {
-      let pos  = &d["position"];
-      let size = &d["size"];
-      Wall::new(
-        pos["x"].as_f32().expect(
-          &format!("Couldn't convert a Wall's position JsonValue 'x' to a f32: {:?}", pos["x"])
-        ),
-        pos["y"].as_f32().expect(
-          &format!("Couldn't convert a Wall's position JsonValue 'y' to a f32: {:?}", pos["y"])
-        ),
-        size["w"].as_f32().expect(
-          &format!("Couldn't convert a Wall's size JsonValue 'w' to a f32: {:?}", size["w"])
-        ),
-        size["h"].as_f32().expect(
-          &format!("Couldn't convert a Wall's size JsonValue 'h' to a f32: {:?}", size["h"])
-        )
-      )
-    }).collect()
-  );
-}
 
 pub struct GameState {
   player:        Player,
